a47945a20be45e63ce1b40e49afd3c9f126b3e4e
Daarshik Dwivedi <you@example.com>
2023-08-12 03:39:49 +0530

    First Commit

---
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -0,0 +1,37 @@
+package com.in28minutes.springboot.myfirstwebapp.todo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+	
+	private static List<Todo> todos = new ArrayList<Todo>();
+	private static int todosCount = 0;
+	
+	static {
+		todos.add(new Todo(++todosCount, "in28minutes", "Learn AWS", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(++todosCount, "in28minutes", "Learn AWS", LocalDate.now().plusYears(1), false));
+	}
+	
+	public List<Todo> findByUsername(String username) {
+		return todos;
+	}
+	
+	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+		Todo todo = new Todo(++todosCount, username, description, targetDate, done);
+		todos.add(todo);
+	}
+	
+	public void deleteById(int id) {
+		//lamda functions
+		//todo.getId()==id
+		//todo -> todo.getId()==id
+		Predicate<? super Todo> predicate = todo -> todo.getId()==id;
+		todos.removeIf(predicate);
+	}
+}
